e94e6f18aa305f69f81d73d52c7684bb89333c5a
Carl Harris <ceharris@vt.edu>
2014-04-07 18:13:31 -0400

    added JPA annotations to VlanEntity type

--- vlan-manager/src/main/java/mtm/vlanmgr/domain/VlanEntity.java
+++ vlan-manager/src/main/java/mtm/vlanmgr/domain/VlanEntity.java
@@ -1,11 +1,32 @@
 package mtm.vlanmgr.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
 import mtm.vlanmgr.Vlan;
 
+@Entity
+@Table(name = "vlan")
 public class VlanEntity implements Vlan {
 
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Version
+  private Long version;
+  
+  @Column(name = "vlan_id", nullable = false, unique = true)
   private Integer vlanId;
-  private String name;  
+  
+  @Column(nullable = false, unique = true)
+  private String name;
+  
+  @Column
   private String note;
 
   @Override
