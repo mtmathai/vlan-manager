5d93bb76c8f11e6ddf3fc54c031191a22cf228a0
Carl Harris <ceharris@vt.edu>
2014-04-25 09:34:44 -0400

    do over

--- src/main/java/mtm/vlanmgr/repository/VlanRepository.java
+++
@@ -1,8 +0,0 @@
-package mtm.vlanmgr.repository;
-
-import mtm.vlanmgr.Vlan;
-
-public interface VlanRepository {
-	void add(Vlan vlan) throws RepositoryException;
-
-}
--- src/main/java/mtm/vlanmgr/service/AddVlanException.java
+++
@@ -1,8 +0,0 @@
-package mtm.vlanmgr.service;
-
-public class AddVlanException extends Exception {
-
-	private static final long serialVersionUID = 1684514566934730270L;
-	
-
-}
