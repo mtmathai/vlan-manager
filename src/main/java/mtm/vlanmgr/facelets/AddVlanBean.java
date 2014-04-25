060feed698c73143ba74ca49ea8fe60924a2f478
Carl Harris <ceharris@vt.edu>
2014-04-25 10:05:33 -0400

    small rewind to remove implementations from bean until test suite ready

--- src/main/java/mtm/vlanmgr/facelets/AddVlanBean.java
+++ src/main/java/mtm/vlanmgr/facelets/AddVlanBean.java
@@ -1,6 +1,5 @@
 package mtm.vlanmgr.facelets;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -8,7 +7,6 @@ import javax.inject.Named;
 import mtm.vlanmgr.service.AddVlanService;
 import mtm.vlanmgr.service.Errors;
 import mtm.vlanmgr.service.VlanEditor;
-import mtm.vlanmgr.service.VlanException;
 
 @Named
 @ConversationScoped
@@ -21,24 +19,14 @@ public class AddVlanBean {
   protected Errors errors;
   
   private VlanEditor vlan;
-
-  @PostConstruct
-  public void init() {
-    vlan = addVlanService.createEditor();
-  }
   
   public VlanEditor getVlan() {
     return vlan;
   }
 
   public String save() {
-    try {
-      addVlanService.saveVlan(vlan, errors);
-      return "success";
-    }
-    catch (VlanException ex) {
-      return null;
-    }
+    // TODO
+    return null;
   }
   
 }
